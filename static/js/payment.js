af72fd3fdfc8e524803f868b21658ff10416f449
RobertoGonzalez <roberto@aerolab.com.ar>
2014-10-22 13:10:44 -0300

    Quick checkout attempt (untested)

--- static/js/payment.js
+++ static/js/payment.js
@@ -3,24 +3,47 @@
  * Powered by Mango
  */
 
+$('.button-get-tickets').click(function(event){
+  event.preventDefault();
+  $('.modal-holder').show();
+});
+
+$('.modal-close').click(function(event){
+  event.preventDefault();
+  $('#modal-holder').hide();
+});
+
+
 ;(function(){
 
-  $('#payment-button').click(function(){
+  $('#payment-form').submit(function(event){
+    event.preventDefault();
+
+    if( $(this).hasClass('loading') ) {
+      return false;
+    }
+
+    $(this).addClass('loading');
+
     $.ajax({
       'method': 'POST',
       'url': '/mango/authorization',
       'data': { 
-        'cardtype': $('#cardtype').val(), 
-        'email': $('#email').val() 
+        'email': $('#payment-email').val(),
+        'cardtype': $('#payment-cardtype').val()
       },
       'success': function(data){
 
         if( typeof data.salesError !== 'undefined' ) {
           // Something went wrong (there are no tickets available or the sale hasn't started yet)
+          $('#payment-form').removeClass('loading');
         } else {
           initPayment(data);
         }
-
+      },
+      'error': function(data){
+        alert("We had an issue while setting up your payment. Try again!");
+        $('#payment-form').removeClass('loading');
       }
     });
   });
@@ -30,10 +53,17 @@
       'api_key': 'public_test_wsl91zxbk055ao3d', 
       'authorization_uid': data.uid
     }, function(error){
+
       if(error) {
         //handle error
+        alert("We had an issue while setting up your payment. Try again!");
+        $('#payment-form').removeClass('loading');
         return;
       }
+
+      // Hide the input modal and show the Mango one
+      $('#payment-form').removeClass('loading');
+      $('#modal-holder').hide();
 
       this.charge(confirmPayment);
     });
@@ -41,7 +71,9 @@
 
   function confirmPayment(error, data) {
     if(error) {
-      // handle error
+      // Todo: handle error
+      alert("There was an issue when processing your payment. We'll contact you to sort out the details, but feel free to try again with another card.");
+      $('#payment-form').removeClass('loading');
       return;
     }
 
@@ -51,9 +83,11 @@
       'data': { 'charge_uid': data.charge_uid },
       'success': function(res) {
         if(res === 'ok') {
-          // success
+          // Todo: success
+          alert("Awesome! You'll receive an email with your ticket.");
         } else {
           // error
+          alert("There was an issue when processing your payment. We'll contact you to sort out the details.");
         }
       } 
     });
