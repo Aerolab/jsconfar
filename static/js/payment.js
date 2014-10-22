4ae0b35d3714630de1c93c353f45fcd9c43b49e2
RobertoGonzalez <roberto@aerolab.com.ar>
2014-10-22 17:16:05 -0300

    Nicer payment errors.

--- static/js/payment.js
+++ static/js/payment.js
@@ -1,33 +1,6 @@
 /**
  * Ticket Sales
  */
-
-$('.button-get-tickets').click(function(event){
-  event.preventDefault();
-  $('.modal-holder').show();
-});
-
-$('.modal-close').click(function(event){
-  event.preventDefault();
-  if( $('#payment-form').hasClass('loading') ) {
-    // Don't close
-    alert("Hold on, we're setting up the payment. Wait 30 seconds and refresh the page if it's still loading.");
-  } else {
-    $('#modal-holder').hide();
-  }
-});
-
-
-// Prevent closing the tab if the payment form is processing
-window.onbeforeunload = function (event) {
-    var event = event || window.event;
-
-    if( $('#payment-form').hasClass('loading') ) {
-      return "If you close the tab while processing the payment you'll have to start again.";
-    }
-};
-
-
 ;(function(){
 
   $('#payment-form').submit(function(event){
@@ -51,7 +24,7 @@ window.onbeforeunload = function (event) {
 
         if( typeof data.salesError !== 'undefined' ) {
           // Something went wrong (there are no tickets available or the sale hasn't started yet)
-          alert( data.salesError );
+          showError( data.salesError );
 
           $('#payment-form').removeClass('loading');
           return;
@@ -61,7 +34,7 @@ window.onbeforeunload = function (event) {
 
       },
       'error': function(data){
-        alert("There are some connectivity issues and we can't get started with your payment. Try again in 30 seconds!");
+        showError("There are some connectivity issues and we can't get started with your payment.<br/>Try again in 30 seconds!");
         $('#payment-form').removeClass('loading');
       }
     });
@@ -75,7 +48,7 @@ window.onbeforeunload = function (event) {
 
       if(error) {
         //handle error
-        alert("We had an issue while setting up your payment. Try again!");
+        showError("We had an issue while setting up your payment. Try again!");
         $('#payment-form').removeClass('loading');
         return;
       }
@@ -92,7 +65,7 @@ window.onbeforeunload = function (event) {
 
     if(error) {
       // Todo: handle error
-      alert("There was an issue when processing your payment. Your card has not been charged. We'll contact you to sort out the details, but feel free to try again with another card.");
+      showError("There was an issue when processing your payment. Your card has not been charged. We'll contact you to sort out the details, but feel free to try again with another card.");
       $('#payment-form').removeClass('loading');
       return;
     }
@@ -104,21 +77,77 @@ window.onbeforeunload = function (event) {
       'success': function(res) {
         if(res === 'ok') {
           // Todo: success
-          alert("Awesome! You'll receive an email with your ticket.");
+          showSuccess("Awesome! You'll receive an email with your ticket.");
         } else {
           // error
-          alert("There was an issue when processing your payment. Your card has not been charged. We'll contact you to sort out the details, but feel free to try again with another card.");
+          showError("There was an issue when processing your payment. Your card has not been charged. We'll contact you to sort out the details, but feel free to try again with another card.");
         }
 
         $('#payment-form').removeClass('loading');
         $('#modal-holder').hide();
       },
       'error': function() {
-        alert("There are some connectivity issues and we can't verify if your payment was made. Contact us at support@jsconfar.com.");
+        showError("There are some connectivity issues and we can't verify if your payment was made. Contact us at <a href=\"mailto:support@jsconfar.com\">support@jsconfar.com</a>.");
         $('#payment-form').removeClass('loading');
         $('#modal-holder').show();
       }
     });
   }
+
+
+
+  function showSuccess(message) {
+    $('#payment-form').removeClass('loading');
+    $('#modal-holder > .modal').hide();
+
+    $('#modal-holder > .modal-success h2').html(message);
+    $('#modal-holder > .modal-success').show();
+  }
+
+
+  function showError(message) {
+    $('#payment-form').removeClass('loading');
+    $('#modal-holder > .modal').hide();
+
+    $('#modal-holder > .modal-error h2').html(message);
+    $('#modal-holder > .modal-error').show();
+  }
+
+
+
+  $('.button-get-tickets').click(function(event){
+    event.preventDefault();
+    $('#modal-holder > .modal').hide();
+    $('#modal-holder > .modal-payment').show();
+    $('.modal-holder').fadeIn(200);
+  });
+
+  $('.modal-close').click(function(event){
+    event.preventDefault();
+    if( $('#payment-form').hasClass('loading') ) {
+      // Don't close
+      alert("Hold on, we're setting up the payment. Wait 30 seconds and refresh the page if it's still loading.");
+    } else {
+      $('#modal-holder > .modal').hide();
+      $('#modal-holder > .modal-payment').show();
+      $('#modal-holder').hide();
+    }
+  });
+
+  $('.modal-success button, .modal-error button').click(function(event){
+    $('.modal-close').trigger('click');
+  });
+
+
+  // Prevent closing the tab if the payment form is processing
+  window.onbeforeunload = function (event) {
+      var event = event || window.event;
+
+      if( $('#payment-form').hasClass('loading') ) {
+        return "If you close the tab while processing the payment you'll have to start again.";
+      }
+  };
+
+
 
 })();
